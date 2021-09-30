a3597be86007ad48ebcdf46ac84a2052777175b1
FranciscoKeele <fkeele2015@gmail.com>
2021-09-30 17:36:34 -0600

    git test after computer crash

--- 1 - VB/PhonebookApp/Contact.vb
+++ 1 - VB/PhonebookApp/Contact.vb
@@ -1,13 +1,17 @@
 Class Contact
-    Property Name As string
-    Property PhoneNumber As integer
-    Public Sub New(ByVal name as string, ByVal number as integer)
+   Property Name As string
+   Property PhoneNumber As Integer 
+   Public Sub New(ByVal name as string, ByVal number as Integer)
         Me.Name = name
         Me.PhoneNumber = number
+    End sub
+    Public Sub New()
+        'x parameterless constructor
     End Sub
-    
-    Overrides Function ToString()  As string
-        return $"Name: {Me.Name} Number: {Me.PhoneNumber}"        
+
+
+    Overrides Function ToString() As string
+        return $"Name: {Me.Name} Number: {Me.PhoneNumber}"
     End Function
-    
+
 End Class
--- 1 - VB/PhonebookApp/Mainmenu.vb
+++ 1 - VB/PhonebookApp/Mainmenu.vb
@@ -1,19 +1,20 @@
-Imports System.io
+Imports System.IO
 Imports System.Text.Json
 
 Class MainMenu
-    Implements  IMenu
+    Implements IMenu
     dim friends as List(of Contact) = new List(of Contact)
     dim filename as string = "friends.json"
     dim jsonstring as string = ""
-    Sub Start() Implements IMenu.Start
+    sub Initialize() Implements Imenu.Start
         dim repeat as Boolean = true
         Do While repeat
             Console.WriteLine("What would you like to do?")
             Console.WriteLine("[0] Add a friend")
-            Console.WriteLine("[1] show friends")
+            Console.WriteLine("[1] Show all friends")
             Console.WriteLine("[x] Exit")
-            Dim input as string = Console.ReadLine()
+            
+            dim input as string = Console.ReadLine()
             Select Case input
                 Case "0"
                     Console.WriteLine("Hello")
@@ -26,38 +27,38 @@ Class MainMenu
             End Select
         Loop
     End Sub
+    
     Sub AddFriend()
         Console.WriteLine("Name: ")
-        dim name as string = console.ReadLine()
+        dim name as string = Console.ReadLine()
         Console.WriteLine("Number: ")
-        dim number as string = console.ReadLine()  
-        dim newFriend as Contact = new Contact(name, Int32.Parse(number))     
+        dim number as string = Console.ReadLine()
+        dim newFriend as Contact = new Contact(name, int32.Parse(number))
         AddFriend2File(newFriend)
-        Console.WriteLine("New Friend Created  "+ newFriend.ToString())
+        Console.WriteLine("New Friend Created: " + newFriend.ToString())
     End Sub
     
     Sub ShowFriends()
-        Console.WriteLine("Friends List plus contact info")
+        Console.WriteLine("Friend list plus contact info")
         For Each person As Contact In GetContactsFromFile()
             Console.WriteLine(person.ToString())
         Next 
-    End Sub
-
+    End sub
+    
     Sub AddFriend2File(ByVal person as Contact)
         dim existingContacts as List(of Contact) = GetContactsFromFile()
         existingContacts.Add(person)
         jsonstring = JsonSerializer.Serialize(existingContacts)
         File.WriteAllText(filename, jsonstring)
     End Sub
-
+    
     Function GetContactsFromFile() As List(of Contact)
         Try
-            jsonString = File.ReadAllText(filename)
-            return JsonSerializer.Deserialize(of List(of Contact)) (jsonstring)
-        Catch ex As Exception
+            jsonstring = File.ReadAllText(filename)
+            return JsonSerializer.Deserialize(of List(of Contact))(jsonstring)
+        catch ex As Exception
             return new List(of Contact)
         End Try
-        
-    End Function    
+    End Function
 
 End Class
